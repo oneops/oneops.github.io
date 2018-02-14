fc05bc84a659a0cb4e5c4cfaab9be4f2898f21a4
Manfred Moser <manfred@simpligility.com>
2018-02-13 16:32:05 -0800

    removed utility script

--- linkcheck.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Running linkchecker on http://localhost:4000"
-linkchecker --anchors http://localhost:4000 > linkcheck-results.log
-cat linkcheck-results.log
-echo "Completed. Results also stored in linkcheck-results.log"
